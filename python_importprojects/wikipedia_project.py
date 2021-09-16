6f80b4a21efc1c9df9e0abff0d93f2ed88b0b64f
Del <mBookpro2021133>
2021-09-15 21:12:33 -0600

    Final edits

--- python_importprojects/wikipedia_project.py
+++ python_importprojects/wikipedia_project.py
@@ -2,7 +2,6 @@ import wikipedia
 from wikipedia.wikipedia import search
 
 # finding result for the search
-# sentences = 2 refers to numbers of line
 search = input("Enter a search topic:")
 result = wikipedia.summary({search}, sentences = 2) 
   
