d0e98761a6731ee7659404978c335fa2a472c2d5
Del <mBookpro2021133>
2021-09-15 21:08:09 -0600

    Complete Assignment

--- python_importprojects/wikipedia_project.py
+++ python_importprojects/wikipedia_project.py
@@ -1,1 +1,10 @@
-print("print")
+import wikipedia
+from wikipedia.wikipedia import search
+
+# finding result for the search
+# sentences = 2 refers to numbers of line
+search = input("Enter a search topic:")
+result = wikipedia.summary({search}, sentences = 2) 
+  
+# printing the result
+print(result)
