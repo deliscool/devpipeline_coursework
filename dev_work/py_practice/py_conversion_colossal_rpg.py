51b2def67e12f0898b2269428ecf52be50fdc280
Del <mBookpro2021133>
2021-09-22 15:05:32 -0600

    make edits

---
+++ dev_work/py_practice/py_conversion_colossal_rpg.py
@@ -0,0 +1,11 @@
+from simple_chalk import chalk, green
+
+game_over = False
+player_health = 100
+backpack = []
+animal = []
+
+
+
+def animals (animal_name, min_damage, max_damage, health_points, item):
+    
