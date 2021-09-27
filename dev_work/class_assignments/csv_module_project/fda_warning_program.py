632924502566348ddc74d2dcd54ee5bfa31c3b7a
Del <mBookpro2021133>
2021-09-26 23:28:48 -0600

    made columns

--- dev_work/class_assignments/csv_module_project/fda_warning_program.py
+++ dev_work/class_assignments/csv_module_project/fda_warning_program.py
@@ -4,7 +4,7 @@ warning_letter = '483_2020.csv'
 
 with open(warning_letter) as csv_file:
  
-        # reading the csv file using DictReader
+    # reading the csv file using DictReader
     csv_reader = csv.DictReader(csv_file)
  
     # converting the file to dictionary
@@ -14,6 +14,16 @@ with open(warning_letter) as csv_file:
  
     # making a list from the keys of the dict
     list_of_column_names = list(dict_from_csv.keys())
- 
+    
+    #display column title by index
+    posted_date = list_of_column_names[0]
+    letter_sent = list_of_column_names[1]
+    company_name = list_of_column_names[2]
+    issuing_office = list_of_column_names[3]
+    subject = list_of_column_names[4]
+    response_letter = list_of_column_names[5]
+    close_out = list_of_column_names[5]
+
     # displaying the list of column names
-    print(list_of_column_names)
+    print(f'{posted_date:>0}{company_name:>18} {issuing_office:>22} {subject:>19} {response_letter:>25}')
+    print("-----------      ------------         --------------     ------------------------  ---------------")
